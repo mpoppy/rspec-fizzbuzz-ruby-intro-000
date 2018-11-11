b7a0dec36d3ca8398d26a5b699cb03210c9633d2
Marc Poppy <poppy.marc@gmail.com>
2018-11-11 00:57:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-#  if int % 3 == 0
-#    print "Fizz"
-#  elsif int % 5 == 0
+ if int % 3 == 0
+  print "Fizz"
+#   elsif int % 5 == 0
 #    print "Buzz"
 #  elsif int % 15 == 0
 #    print "FizzBuzz"
-#  else
-#    return nil
+  else
+    return nil
 end
